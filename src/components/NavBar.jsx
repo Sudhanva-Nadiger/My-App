9ea9e8ef69d5f8294b1c9e951e6abc4070ac58cf
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2023-01-09 21:15:00 +0530

    feat NavBar
    - Outline of NavBar
    - add dev-dependency '@mui/icons-material'
    - remove margin of <body>

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
+import Logo from '../assets/vite.svg'
+import {AppBar, Box, IconButton, Link, Toolbar} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+
+const noop = () => {}
 
 const NavBar = () => {
   return (
-    <div>
-      //logo
-
-      //link: home, history
-    </div>
+    <AppBar sx={{ position: 'relative' }}>
+      <Toolbar>
+        <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={noop}
+            sx={{ display: { md: 'none' } }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Box sx={{ p: '0.5rem'}}>
+          <img src={Logo} alt="Vite" />
+        </Box>
+        <Toolbar sx={{ px: '1rem', width: '100%' }}>
+          <Link href="#" sx={{ color: 'inherit', p: '0.5rem' }}>Home</Link>
+          <Link href="#" sx={{ color: 'inherit', p: '0.5rem' }}>History</Link>
+        </Toolbar>
+      </Toolbar>
+    </AppBar>
   )
 }
 
